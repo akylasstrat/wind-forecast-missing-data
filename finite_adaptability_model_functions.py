fixed many errors, gradient-descent algo works good now fixed also errors in finite adaptability

finite_adaptability_model_functions.py
@@ -1150,9 +1150,11 @@ class depth_Finite_FDRR(object):
                 node = self.children_right_dict[node]
              #print('New Node: ', node)
          if (m0==self.missing_pattern[node]).all():
+
              # nominal model
              Predictions.append( x0@self.node_coef_[node] + self.node_bias_[node] )
          else:
+
              # WC model
              Predictions.append( x0@self.wc_node_coef_[node] + self.wc_node_bias_[node] )
      return np.array(Predictions)
@@ -1250,27 +1252,30 @@ class FiniteAdaptability_MLP(object):
 
     
     mlp_model = gd_FDRR(input_size = num_features, hidden_sizes = self.gd_FDRR_params['hidden_sizes'], output_size = self.gd_FDRR_params['output_size'], 
-                              target_col = self.target_features[0], fix_col = self.fixed_features[0], projection = self.gd_FDRR_params['projection'], train_adversarially = False)
+                              target_col = self.target_features[0], fix_col = self.fixed_features[0], projection = self.gd_FDRR_params['projection'], 
+                              train_adversarially = False, budget_constraint = self.gd_FDRR_params['budget_constraint'])
     
     optimizer = torch.optim.Adam(mlp_model.parameters(), lr = self.MLP_train_dict['lr'])
     mlp_model.train_model(train_data_loader, valid_data_loader, optimizer, 
-                          epochs = self.MLP_train_dict['epochs'], patience = self.MLP_train_dict['patience'], verbose = self.MLP_train_dict['verbose'])
+                          epochs = self.MLP_train_dict['epochs'], patience = self.MLP_train_dict['patience'], 
+                          verbose = self.MLP_train_dict['verbose'])
         
     ######## Train Adversarially Robust model
     # budget of robustness
     gamma_temp = len(self.target_features[0])
 
     robust_mlp_model = gd_FDRR(input_size = num_features, hidden_sizes = self.gd_FDRR_params['hidden_sizes'], output_size = self.gd_FDRR_params['output_size'], 
-                              target_col = self.target_features[0], fix_col = self.fixed_features[0], projection = self.gd_FDRR_params['projection'], train_adversarially = True, 
-                              Gamma = gamma_temp)
+                              target_col = self.target_features[0], fix_col = self.fixed_features[0], projection = self.gd_FDRR_params['projection'], 
+                              train_adversarially = True, 
+                              Gamma = gamma_temp, budget_constraint = self.gd_FDRR_params['budget_constraint'])
 
     optimizer = torch.optim.Adam(robust_mlp_model.parameters(), lr = self.MLP_train_dict['lr'])
     
-    robust_mlp_model.load_state_dict(mlp_model.state_dict(), strict=False)
+    # robust_mlp_model.load_state_dict(mlp_model.state_dict(), strict=False)
 
     robust_mlp_model.train_model(train_data_loader, valid_data_loader, optimizer, 
                           epochs = self.MLP_train_dict['epochs'], patience = self.MLP_train_dict['patience'], verbose = self.MLP_train_dict['verbose'], 
-                          warm_start = True)
+                          warm_start = False, attack_type = self.gd_FDRR_params['attack_type'])
     
 
     # Nominal and WC loss
@@ -1440,15 +1445,16 @@ class FiniteAdaptability_MLP(object):
 
             left_robust_mlp_model = gd_FDRR(input_size = num_features, hidden_sizes = self.gd_FDRR_params['hidden_sizes'], output_size = self.gd_FDRR_params['output_size'], 
                                       target_col = left_target_cols, fix_col = left_fix_cols, projection = self.gd_FDRR_params['projection'], 
-                                      train_adversarially = True, Gamma = gamma_temp)
+                                      train_adversarially = True, Gamma = gamma_temp, 
+                                      budget_constraint = self.gd_FDRR_params['budget_constraint'])
 
             optimizer = torch.optim.Adam(left_robust_mlp_model.parameters(), lr = self.MLP_train_dict['lr'])
             
-            left_robust_mlp_model.load_state_dict(self.node_model_[node].state_dict(), strict=False)
+            # left_robust_mlp_model.load_state_dict(self.node_model_[node].state_dict(), strict=False)
 
             left_robust_mlp_model.train_model(left_train_data_loader, left_valid_data_loader, optimizer, 
                                   epochs = self.MLP_train_dict['epochs'], patience = self.MLP_train_dict['patience'], verbose = self.MLP_train_dict['verbose'], 
-                                  warm_start = False)
+                                  warm_start = False, attack_type = self.gd_FDRR_params['attack_type'])
 
             
             # Estimate WC loss and nominal loss
@@ -1481,27 +1487,28 @@ class FiniteAdaptability_MLP(object):
             gamma_temp = len(right_target_cols)
             
             temp_miss_X = (1-right_missing_pattern)*X
-            
             if val_split == 0:    
                 tensor_train_missX = torch.FloatTensor(temp_miss_X)                        
+                tensor_valid_missX = tensor_train_missX
             else:
-                tensor_trainY = torch.FloatTensor(Y[:-n_valid_obs])
-                tensor_validY = torch.FloatTensor(Y[-n_valid_obs:])
-
+                tensor_train_missX = torch.FloatTensor(temp_miss_X[:-n_valid_obs])
+                tensor_valid_missX = torch.FloatTensor(temp_miss_X[-n_valid_obs:])
+                
             right_train_data_loader = create_data_loader([tensor_train_missX, tensor_trainY], batch_size = self.MLP_train_dict['batch_size'])        
             right_valid_data_loader = create_data_loader([tensor_valid_missX, tensor_validY], batch_size = self.MLP_train_dict['batch_size'])
 
             right_robust_mlp_model = gd_FDRR(input_size = num_features, hidden_sizes = self.gd_FDRR_params['hidden_sizes'], output_size = self.gd_FDRR_params['output_size'], 
                                       target_col = right_target_cols, fix_col = right_fix_cols, projection = self.gd_FDRR_params['projection'], 
-                                      train_adversarially = True, Gamma = gamma_temp)
+                                      train_adversarially = True, 
+                                      Gamma = gamma_temp, budget_constraint = self.gd_FDRR_params['budget_constraint'])
 
             optimizer = torch.optim.Adam(right_robust_mlp_model.parameters(), lr = self.MLP_train_dict['lr'])
             
-            right_robust_mlp_model.load_state_dict(new_mlp_model.state_dict(), strict=False)
+            right_robust_mlp_model.load_state_dict(self.wc_node_model_[node].state_dict(), strict=False)
 
             right_robust_mlp_model.train_model(right_train_data_loader, right_valid_data_loader, optimizer, 
                                   epochs = self.MLP_train_dict['epochs'], patience = self.MLP_train_dict['patience'], verbose = self.MLP_train_dict['verbose'], 
-                                  warm_start = False)
+                                  warm_start = False, attack_type = self.gd_FDRR_params['attack_type'])
 
             # Estimate WC loss and nominal loss
             right_insample_wcloss = eval_predictions(right_robust_mlp_model.predict(tensor_train_missX), Y, self.error_metric)
@@ -1519,8 +1526,8 @@ class FiniteAdaptability_MLP(object):
             self.wc_node_model_.append(right_robust_mlp_model)
                         
             # update missing patterns for downstream robust problem            
-            self.fixed_features.append(left_fix_cols)
-            self.target_features.append(left_target_cols)
+            self.fixed_features.append(right_fix_cols)
+            self.target_features.append(right_target_cols)
                         
             if node==0:
                 self.children_left[node] = node_id_counter+1
@@ -1530,7 +1537,7 @@ class FiniteAdaptability_MLP(object):
                 self.children_right.append(node_id_counter+2)
 
             self.children_left_dict[node] = node_id_counter+1
-            self.children_right_dict[node] = node_id_counter+1
+            self.children_right_dict[node] = node_id_counter+2
 
             # Update lists of candidate nodes (node is removed in both cases)
             list_nodes_candidates = list_nodes_candidates + [ node_id_counter + 1, node_id_counter + 2]
@@ -1603,6 +1610,7 @@ class FiniteAdaptability_MLP(object):
          m0 = missing_mask[i:i+1,:]
          #Start from root node
          node = 0
+
          # !!!! If you go to leaf, might be overly conservative
          #Go down the tree until you match the missing pattern OR a Leaf node is reached
          while ((self.children_left_dict[node] != -1) and (self.children_right_dict[node] != -1)):
@@ -1616,13 +1624,17 @@ class FiniteAdaptability_MLP(object):
                  # if feature is missing, go right
                 node = self.children_right_dict[node]
              #print('New Node: ', node)
+         
          if (m0==self.missing_pattern[node]).all():
              # nominal model
-             Predictions.append( self.node_model_[node].predict(torch.FloatTensor(x0)))
+             print(f'Nominal Node:{node}')
+             Predictions.append( self.node_model_[node].predict(torch.FloatTensor(x0)).reshape(-1))
          else:
+             print(f'WC Node:{node}')
              # WC model
-             Predictions.append( self.wc_node_model_[node].predict(torch.FloatTensor(x0)))
-     return np.array(Predictions)
+             Predictions.append( self.wc_node_model_[node].predict(torch.FloatTensor(x0)).reshape(-1))
+
+     return np.array(Predictions).reshape(-1,1)
 
 
 ###### Auxiliary functions

testing_gd_algo.py
@@ -284,6 +284,7 @@ target_col = [np.where(Predictors.columns == c)[0][0] for c in target_pred]
 fix_col = []
 
 #%%%% FDDR-AAR: train one model per value of \Gamma
+from FDR_regressor_test import *
 
 case_folder = config['store_folder']
 output_file_name = f'{cd}\\{case_folder}\\trained-models\\{target_park}_fdrr-aar_{max_lag}.pickle'
@@ -296,6 +297,8 @@ fix_col = []
 K_parameter = np.arange(0, len(target_pred)+1)
 
 FDRR_AAR_models = []
+ineq_FDRR_AAR_models = []
+
 config['train'] = True
 if config['train']:
     for K in K_parameter:
@@ -308,10 +311,42 @@ if config['train']:
     
         print('FDR: ', eval_point_pred(fdr_pred, Target.values, digits=2))
         FDRR_AAR_models.append(fdr)
+
+
+        ineq_fdr = FDR_regressor_test(K = K)
+        ineq_fdr.fit(trainPred.values, trainY, target_col, fix_col, solution = 'reformulation', budget = 'inequality')              
+        ineq_FDRR_AAR_models.append(ineq_fdr)
+
+        print('FDR: ', eval_point_pred(fdr_pred, Target.values, digits=2))
+        print('ineq_FDR: ', eval_point_pred(ineq_fdr.predict(testPred).reshape(-1,1), Target.values, digits=2))
     
 else:
     with open(output_file_name, 'rb') as handle:    
             FDRR_AAR_models = pickle.load(handle)
+
+#%% 
+
+for K in np.arange(11): 
+    plt.plot(FDRR_AAR_models[K].coef_, label = 'equality')
+    plt.plot(ineq_FDRR_AAR_models[K].coef_, label = 'inequality')
+    plt.legend()
+    plt.show()
+    
+#%%
+K_select = 8
+for K in np.arange(K_select + 1):
+    feat = np.random.choice(target_col, size = K, replace = False)
+    a = np.zeros((1,trainPred.shape[1]))
+    a[:,feat] = 1
+    
+    plt.plot(FDRR_AAR_models[K_select].predict(testPred*(1-a)[:1000]))
+    plt.plot(ineq_FDRR_AAR_models[K_select].predict(testPred*(1-a)[:1000]))
+    plt.show()
+    
+    print(f'Realized number of missing features: {K}')
+    print('FDR: ', eval_point_pred(FDRR_AAR_models[K_select].predict(testPred*(1-a)).reshape(-1,1), Target.values, digits=2)[1])
+    print('ineq_FDR: ', eval_point_pred(ineq_FDRR_AAR_models[K_select].predict(testPred*(1-a)).reshape(-1,1), Target.values, digits=2)[1])
+
 #%%%% Gradient-based FDRR
 
 from torch_custom_layers import * 
@@ -337,8 +372,6 @@ valid_base_data_loader = create_data_loader([tensor_validPred, tensor_validY], b
 n_features = tensor_trainPred.shape[1]
 n_outputs = tensor_trainY.shape[1]
 
-gd_FDR_models = []
-
 # nominal model (no missing data) to warm-start all future models
 nominal_model = gd_FDRR(input_size = n_features, hidden_sizes = [], output_size = n_outputs, 
                           target_col = target_col, fix_col = fix_col, projection = False, Gamma = 0, train_adversarially = False)
@@ -347,116 +380,29 @@ optimizer = torch.optim.Adam(nominal_model.parameters(), lr = 1e-2)
 nominal_model.train_model(train_base_data_loader, valid_base_data_loader, optimizer, epochs = num_epochs, 
                       patience = patience, verbose = 0)
 #%%
-batch_size = 50
+batch_size = 250
+gd_FDR_models = []
+
 from torch_custom_layers import *
 
-test_alpha = torch.tensor([1., 1,1, 0, 1., 1., 1, 1., 1., 1., 0., 0., 1.]).reshape(1,-1)
-
-for K in [10]:
+for K in K_parameter:
     
-    gd_fdr_model = gd_FDRR(input_size = n_features, hidden_sizes = [], output_size = n_outputs, 
+    feat = np.random.choice(target_col, size = K, replace = False)
+    a = np.zeros((1,trainPred.shape[1]))
+    a[:,feat] = 1
+    test_alpha = torch.FloatTensor(a)
+    
+    gd_fdr_model = gd_FDRR(input_size = n_features, hidden_sizes = [20, 20, 20], output_size = n_outputs, 
                               target_col = target_col, fix_col = fix_col, projection = False, 
-                              Gamma = K, train_adversarially = True)
+                              Gamma = K, train_adversarially = True, budget_constraint = 'equality')
     
     optimizer = torch.optim.Adam(gd_fdr_model.parameters(), lr = 1e-2)
 
-    # initialize weights with nominal model
-    gd_fdr_model.load_state_dict(nominal_model.state_dict(), strict=False)
-        
-    gd_fdr_model.train_model(train_base_data_loader, valid_base_data_loader, optimizer, epochs = num_epochs, 
-                          patience = patience, verbose = 0, warm_start = False)
-
-    gd_fdr_pred = gd_fdr_model.predict(tensor_testPred, project = True)
-    
-    gd_FDR_models.append(gd_fdr_model)s
-    
-    print('GD FDRR: ', eval_point_pred(gd_fdr_pred, Target.values, digits=4))
-    print('FDRR: ', eval_point_pred(projection(FDRR_AAR_models[K].predict(testPred).reshape(-1,1)), Target.values, digits=4))
-    
-    plt.plot(gd_fdr_model.predict(tensor_testPred*(1-test_alpha))[:1000], label='GD')
-    plt.plot(projection(FDRR_AAR_models[K].predict(testPred*(1-test_alpha.detach().numpy()))[:1000]), label='MinMax')
-    plt.legend()
-    plt.show()
-
-    for name, param in gd_fdr_model.named_parameters():
-        if param.requires_grad:
-            plt.plot(param.data.detach().numpy().T)
-    plt.plot(FDRR_AAR_models[K].coef_)
-    plt.show()
-
-#%%
-
-# Version two
-batch_size = 50
-
-X = tensor_trainPred
-y = tensor_trainY
-
-for K in [10]:
-    # find a good vertex for adversarial examples
-    y_hat = nominal_model.forward(X)        
-    current_loss = nominal_model.estimate_loss(y_hat, y).mean()
-    # initialize wc loss and adversarial example
-    wc_loss = current_loss.data
-    best_alpha =  torch.zeros_like(X)
-    current_target_col = torch.tensor(target_col)
-    
-    # Iterate over gamma, greedily add one feature per iteration
-    for g in range(K):
-        
-        # store losses for all features
-        local_loss = []
-        # placeholders for splitting a column
-        best_col = None
-        apply_split = False  
-        # !!!!! Important to use clone/copy here, else we update both
-        alpha_init = torch.clone(best_alpha)   
-        
-        # Nominal loss for this iteration using previous alpha values
-        y_hat = nominal_model.forward(X*(1-alpha_init))
-        temp_nominal_loss = nominal_model.estimate_loss(y_hat, y).mean().data
-                
-        # loop over target columns (for current node), find worst-case loss:
-        for col in current_target_col:
-            # create adversarial example
-            alpha_temp = torch.clone(alpha_init)
-            
-            # set feature to missing
-            alpha_temp[:,col] = 1
-                
-            # predict using adversarial example
-            y_hat = nominal_model.forward(X*(1-alpha_temp))
-            temp_loss = nominal_model.estimate_loss(y_hat, y).mean().data
-            local_loss.append(temp_loss.data)
-
-        best_col_ind = np.argsort(local_loss)[-1]                
-        wc_loss = np.max(local_loss)
-        best_col = current_target_col[best_col_ind]
-
-        if wc_loss > temp_nominal_loss:
-            # update
-            best_alpha[:,best_col] = 1
-            apply_split = True
-            
-        #update list of eligible columns
-        if apply_split:
-            current_target_col = torch.cat([current_target_col[0:best_col_ind], current_target_col[best_col_ind+1:]])   
-        else:
-            break
-    sadf
-    train_base_data_loader = create_data_loader([tensor_trainPred*(1-best_alpha), tensor_trainY], batch_size = batch_size, shuffle = True)
-    valid_base_data_loader = create_data_loader([tensor_validPred*(1-best_alpha), tensor_validY], batch_size = batch_size, shuffle = True)
-                
-    gd_fdr_model = gd_FDRR(input_size = n_features, hidden_sizes = [], output_size = n_outputs, 
-                              target_col = target_col, fix_col = fix_col, projection = False, Gamma = K, train_adversarially=False)
-    
-    optimizer = torch.optim.Adam(gd_fdr_model.parameters(), lr = 1e-3)
-
-    # initialize weights with nominal model
+    # initialize weights with nominal model (Does not affect solution much)
     # gd_fdr_model.load_state_dict(nominal_model.state_dict(), strict=False)
-
+        
     gd_fdr_model.train_model(train_base_data_loader, valid_base_data_loader, optimizer, epochs = num_epochs, 
-                          patience = patience, verbose = 0, warm_start = False)
+                          patience = patience, verbose = 0, warm_start = False, attack_type = 'random_sample')
 
     gd_fdr_pred = gd_fdr_model.predict(tensor_testPred, project = True)
     
@@ -464,20 +410,62 @@ for K in [10]:
     
     print('GD FDRR: ', eval_point_pred(gd_fdr_pred, Target.values, digits=4))
     print('FDRR: ', eval_point_pred(projection(FDRR_AAR_models[K].predict(testPred).reshape(-1,1)), Target.values, digits=4))
+    print('ineq-FDRR: ', eval_point_pred(projection(ineq_FDRR_AAR_models[K].predict(testPred).reshape(-1,1)), Target.values, digits=4))
     
-    plt.plot(gd_fdr_model.predict(tensor_testPred*(1-best_alpha[0:1]))[:1000], label='GD')
-    plt.plot(projection(FDRR_AAR_models[K].predict(testPred*(1-best_alpha.detach().numpy()[0:1]))[:1000]), label='MinMax')
+    plt.plot(gd_fdr_model.predict(tensor_testPred*(1-test_alpha))[:1000], label='GD')
+    plt.plot(projection(FDRR_AAR_models[K].predict(testPred*(1-a))[:1000]), label='Eq-MinMax')
+    plt.plot(projection(ineq_FDRR_AAR_models[K].predict(testPred*(1-a))[:1000]), label='Ineq-MinMax')
     plt.legend()
     plt.show()
 
-    for name, param in gd_fdr_model.named_parameters():
-        if param.requires_grad:
-            plt.plot(param.data.detach().numpy().T)
-    plt.plot(FDRR_AAR_models[K].coef_)
+    for layer in gd_fdr_model.model.children():
+        if isinstance(layer, nn.Linear):    
+            plt.plot(layer.weight.data.detach().numpy().T, label = 'GD')
+    plt.plot(FDRR_AAR_models[K].coef_, label='Eq-MinMax')
+    plt.plot(ineq_FDRR_AAR_models[K].coef_, label='Ineq-MinMax')
+    plt.legend()
     plt.show()
 
+#%% Different iterations of Finite Adaptive 
 
-            
+from FiniteRetrain import *
+from FiniteRobustRetrain import *
+from finite_adaptability_model_functions import *
+
+fin_retrain_model = depth_Finite_FDRR(Max_models = 10, D = 20, red_threshold = 0.1, max_gap = 0.25)
+fin_retrain_model.fit(trainPred.values, trainY, target_col, fix_col, tree_grow_algo = 'leaf-wise', 
+                      budget = 'inequality', solution = 'reformulation')
+
+#%% Finite adaptability with Gradient-based algorithm
+
+from FiniteRetrain import *
+from FiniteRobustRetrain import *
+from finite_adaptability_model_functions import *
+from torch_custom_layers import *
+
+# Standard MLPs (separate) forecasting wind production and dispatch decisions
+n_features = tensor_trainPred.shape[1]
+n_outputs = tensor_trainY.shape[1]
+
+#optimizer = torch.optim.Adam(res_mlp_model.parameters())
+
+batch_size = 250
+num_epochs = 1000
+learning_rate = 1e-2
+patience = 20
+
+gd_fin_retrain_model = FiniteAdaptability_MLP(target_col = target_col, fix_col = fix_col, Max_models = 10, D = 20, red_threshold = 0.1, 
+                                            input_size = n_features, hidden_sizes = [], output_size = n_outputs, projection = True, 
+                                            train_adversarially = True, budget_constraint = 'inequality', attack_type = 'greedy', 
+                                            warm_start = False)
+
+gd_fin_retrain_model.fit(trainPred.values, trainY, val_split = 0.0, tree_grow_algo = 'leaf-wise', max_gap = 0.25, 
+                      epochs = num_epochs, patience = patience, verbose = 0, optimizer = 'Adam', 
+                      lr = learning_rate, batch_size = batch_size)
+
+
+
+
 #%%%%%%%%% Testing: varying the number of missing observations/ persistence imputation
 from utility_functions import *
 
@@ -492,11 +480,10 @@ percentage = [0, .001, .005, .01, .05, .1]
 # transition matrix to generate missing data
 P = np.array([[.999, .001], [0.241, 0.759]])
 
-models = ['PERS', 'FDRR-R', 'FDRR-AAR', 'GD-FDRR-AAR', 'RETRAIN', 'FIN-RETRAIN', 'MLP', 
-          'resMLP']
-labels = ['$\mathtt{PERS}$', '$\mathtt{LS}$', '$\mathtt{LS}_{\ell_2}$', '$\mathtt{LS}_{\ell_1}$',
-           '$\mathtt{LAD}$', '$\mathtt{LAD}_{\ell_1}$','$\mathtt{FDRR-R}$', '$\mathtt{FDRR-AAR}$', '$\mathtt{GD-FDRR}$', 
-           '$\mathtt{MLP}$', '$\mathtt{resMLP}$']
+models = ['PERS', 'FDRR', 'ineq-FDRR', 'GD-FDRR', 'FIN-RETRAIN', 'GD-FIN-RETRAIN']
+# labels = ['$\mathtt{PERS}$', '$\mathtt{LS}$', '$\mathtt{LS}_{\ell_2}$', '$\mathtt{LS}_{\ell_1}$',
+#            '$\mathtt{LAD}$', '$\mathtt{LAD}_{\ell_1}$','$\mathtt{FDRR-R}$', '$\mathtt{FDRR-AAR}$', '$\mathtt{GD-FDRR}$', 
+#            '$\mathtt{MLP}$', '$\mathtt{resMLP}$']
 
 
 mae_df = pd.DataFrame(data = [], columns = models+['iteration', 'percentage'])
@@ -600,60 +587,89 @@ for perc in percentage:
             if config['scale']: fdr_pred = target_scaler.inverse_transform(fdr_pred)
             fdr_aar_predictions.append(fdr_pred.reshape(-1))
         fdr_aar_predictions = np.array(fdr_aar_predictions).T
-
-        # Use only the model with the appropriate K
-        final_fdr_aar_pred = fdr_aar_predictions[:,0]
-        if (perc>0)or(config['pattern']=='MNAR'):
-            for j, ind in enumerate(mask_ind):
-                n_miss_feat = miss_X.isna().values[j].sum()
-                final_fdr_aar_pred[j] = fdr_aar_predictions[j, n_miss_feat]
-        final_fdr_aar_pred = final_fdr_aar_pred.reshape(-1,1)
         
-        fdr_aar_mae = eval_predictions(final_fdr_aar_pred, Target.values, metric= error_metric)
+        # FDRR with inequality on budget
+        ineq_fdr_predictions = []
 
-
-        #### FDRR-AAR (select the appropriate model for each case)
-        gd_fdr_aar_predictions = []
         for i, k in enumerate(K_parameter):
-            
-            fdr_pred = gd_FDR_models[i].predict(torch.FloatTensor(miss_X_zero.values)).reshape(-1,1)
+            temp_pred = ineq_FDRR_AAR_models[i].predict(miss_X_zero).reshape(-1,1)
+            temp_pred = projection(temp_pred)
                 
             # Robust
-            if config['scale']: fdr_pred = target_scaler.inverse_transform(fdr_pred)
-            gd_fdr_aar_predictions.append(fdr_pred.reshape(-1))
+            if config['scale']: temp_pred = target_scaler.inverse_transform(temp_pred)
+            ineq_fdr_predictions.append(temp_pred.reshape(-1))
+        ineq_fdr_predictions = np.array(ineq_fdr_predictions).T
+
+
+        gd_fdr_aar_predictions = []
+
+        for i, k in enumerate(K_parameter):
+            gd_fdr_pred = gd_FDR_models[i].predict(torch.FloatTensor(miss_X_zero.values)).reshape(-1,1)                
+            # Robust
+            if config['scale']: 
+                fdr_pred = target_scaler.inverse_transform(gd_fdr_pred)
+            gd_fdr_aar_predictions.append(gd_fdr_pred.reshape(-1))
         gd_fdr_aar_predictions = np.array(gd_fdr_aar_predictions).T
 
         # Use only the model with the appropriate K
+        final_fdr_aar_pred = fdr_aar_predictions[:,0]
         final_gd_fdr_aar_pred = gd_fdr_aar_predictions[:,0]
+        final_ineq_fdr_aar_pred = ineq_fdr_predictions[:,0]
+        
         if (perc>0)or(config['pattern']=='MNAR'):
             for j, ind in enumerate(mask_ind):
                 n_miss_feat = miss_X.isna().values[j].sum()
+                
+                final_fdr_aar_pred[j] = fdr_aar_predictions[j, n_miss_feat]                
                 final_gd_fdr_aar_pred[j] = gd_fdr_aar_predictions[j, n_miss_feat]
+                final_ineq_fdr_aar_pred[j] = ineq_fdr_predictions[j, n_miss_feat]
+
+        final_fdr_aar_pred = final_fdr_aar_pred.reshape(-1,1)
         final_gd_fdr_aar_pred = final_gd_fdr_aar_pred.reshape(-1,1)
+        final_ineq_fdr_aar_pred = final_ineq_fdr_aar_pred.reshape(-1,1)
         
-        gd_fdr_aar_mae = eval_predictions(final_gd_fdr_aar_pred, Target.values, metric= error_metric)
         
+        fdr_aar_mae = eval_predictions(final_fdr_aar_pred, Target.values, metric= error_metric)
+        gd_fdr_aar_mae = eval_predictions(final_gd_fdr_aar_pred, Target.values, metric= error_metric)        
+        ineq_fdr_aar_mae = eval_predictions(final_ineq_fdr_aar_pred, Target.values, metric= error_metric)
+        
+        #### FINITE-RETRAIN
+        
+        fin_retrain_pred = fin_retrain_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
+        fin_retrain_pred = projection(fin_retrain_pred)
+        if config['scale']:
+            fin_retrain_pred = target_scaler.inverse_transform(fin_retrain_pred)            
+        fin_retrain_mae = eval_predictions(fin_retrain_pred.reshape(-1,1), Target.values, metric=error_metric)
+        
+        ##### Gradient-based Finite Retrain
+        gd_fin_retrain_pred = gd_fin_retrain_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int)).reshape(-1,1)
+        gd_fin_retrain_pred = projection(gd_fin_retrain_pred)
+        if config['scale']: gd_fin_retrain_pred = target_scaler.inverse_transform(gd_fin_retrain_pred)            
+        gd_fin_retrain_mae = eval_predictions(gd_fin_retrain_pred.reshape(-1,1), Target.values, metric=error_metric)
+
         temp_df['PERS'] = [pers_mae]                        
-        temp_df['FDRR-AAR'] = fdr_aar_mae
-        temp_df['GD-FDRR-AAR'] = gd_fdr_aar_mae
+        temp_df['FDRR'] = fdr_aar_mae
+        temp_df['GD-FDRR'] = gd_fdr_aar_mae
+        temp_df['ineq-FDRR'] = ineq_fdr_aar_mae
+        temp_df['FIN-RETRAIN'] = fin_retrain_mae
+        temp_df['GD-FIN-RETRAIN'] = gd_fin_retrain_mae
 
         #temp_df['FDRR-CL'] = fdr_cl_mae
         
         mae_df = pd.concat([mae_df, temp_df])
         run_counter += 1
-    
+#%%
 if config['save']:
     mae_df.to_csv(f'{cd}\\{case_folder}\\results\\{target_park}_ID_results.csv')
-    
+
 # Plotting 
 color_list = ['black', 'black', 'gray', 'tab:cyan','tab:green',
          'tab:blue', 'tab:brown', 'tab:purple','tab:red', 'tab:orange', 'tab:olive', 'cyan', 'yellow']
 
-models_to_plot = ['PERS', 'FDRR-AAR', 'GD-FDRR-AAR']
+models_to_plot = ['PERS', 'FDRR', 'ineq-FDRR', 'GD-FDRR', 'FIN-RETRAIN', 'GD-FIN-RETRAIN']
 marker = ['2', 'o', 'd', '^', '8', '1', '+', 's', 'v', '*', '^', 'p', '3', '4']
-labels = ['$\mathtt{PERS}$', '$\mathtt{LS}$', '$\mathtt{LS}_{\ell_2}$', '$\mathtt{LS}_{\ell_1}$',
-           '$\mathtt{LAD}$', '$\mathtt{LAD}_{\ell_1}$','$\mathtt{FDRR}$', '$\mathtt{FDRR-AAR}$', '$\mathtt{GD-FDRR}$', '$\mathtt{FIN-RETRAIN}$', 
-           '$\mathtt{MLP}$', '$\mathtt{resMLP}$']
+labels = ['$\mathtt{PERS}$','$\mathtt{FDRR}$', '$\mathtt{ineq-FDRR}$', '$\mathtt{GD-FDRR}$', '$\mathtt{FIN-RETRAIN}$', 
+          '$\mathtt{GD-FIN-RETRAIN}$']
 
 
 ls_colors = plt.cm.tab20c( list(np.arange(3)))

torch_custom_layers.py
@@ -473,7 +473,7 @@ class resilient_MLP(nn.Module):
     
 class gd_FDRR(nn.Module):        
     def __init__(self, input_size, hidden_sizes, output_size, target_col, fix_col, activation=nn.ReLU(), sigmoid_activation = False, 
-                 projection = False, UB = 1, LB = 0, Gamma = 1, train_adversarially = True):
+                 projection = False, UB = 1, LB = 0, Gamma = 1, train_adversarially = True, budget_constraint = 'inequality'):
         super(gd_FDRR, self).__init__()
         """
         Standard MLP for regression
@@ -482,7 +482,9 @@ class gd_FDRR(nn.Module):
             
             output_size: equal to the number of combination weights, i.e., number of experts we want to combine
             sigmoid_activation: enable sigmoid function as a final layer, to ensure output is in [0,1]
-            
+            budget_constraint: {'inequality', 'equality'}. If 'equality', solution approximates FDRR-reformulation with tight budget constraint.
+            If 'equality', solution approximates FDRR-reformulation with inequality in the budget constraint (Gamma is upper bound). This relaxes the inner max problem, 
+            which makes the robust formulation more pessimistic.
         """
         # Initialize learnable weight parameters
         self.num_features = input_size
@@ -495,6 +497,7 @@ class gd_FDRR(nn.Module):
         self.fix_col = torch.tensor(fix_col, dtype=torch.int32)
         self.gamma = Gamma
         self.train_adversarially = train_adversarially
+        self.budget_constraint = budget_constraint
         
         # create sequential model
         layer_sizes = [input_size] + hidden_sizes + [output_size]
@@ -531,7 +534,10 @@ class gd_FDRR(nn.Module):
         alpha_hat = cp.Parameter((input_size))
         alpha_proj = cp.Variable((input_size), nonneg = True)
     
-        Constraints = [alpha_proj <= 1, alpha_proj.sum() <= self.gamma] 
+        if self.budget_constraint == 'equality':
+            Constraints = [alpha_proj <= 1, alpha_proj.sum() == self.gamma] 
+        elif self.budget_constraint == 'inequality':
+            Constraints = [alpha_proj <= 1, alpha_proj.sum() <= self.gamma] 
                         
         objective_funct = cp.Minimize(  cp.norm(alpha_proj - alpha_hat) ) 
                 
@@ -546,20 +552,19 @@ class gd_FDRR(nn.Module):
             #alpha = torch.rand((1, X.shape[1]), requires_grad=True)
             
             #alpha.data = alpha_init
-            proj_simplex = nn.Softmax()
+            # proj_simplex = nn.Softmax()
+
             optimizer = torch.optim.SGD([alpha], lr=1e-1)
             # print(alpha)
             for t in range(num_iter):
 
-                with torch.no_grad():
-                    pred = self.forward(X*(1-alpha))
+                    
+                pred = self.forward(X*(1-alpha))
 
                 loss = -nn.MSELoss()(pred, y)
                 optimizer.zero_grad()
-                alpha.retain_grad()
-
+                
                 loss.backward()
-
                 optimizer.step()                
                 #alpha.clamp(0,1)              
                 
@@ -589,16 +594,6 @@ class gd_FDRR(nn.Module):
         wc_loss = current_loss.data
         best_alpha =  torch.zeros_like(X)
         current_target_col = self.target_col
-
-        # # find gamma features with higher weight        
-        # for layer in self.model.children():
-        #     if isinstance(layer, nn.Linear):                
-        #         w_coef_param = to_np(layer.weight)
-                
-        # col_ind = np.argsort(np.abs(w_coef_param[0]))[::-1][:gamma]
-        # for c in col_ind:   best_alpha[c] = 1
-
-        # print(f'Budget Gamma={gamma}')
         
         # Iterate over gamma, greedily add one feature per iteration
         for g in range(gamma):
@@ -618,8 +613,6 @@ class gd_FDRR(nn.Module):
             temp_nominal_loss = self.estimate_loss(y_hat, y).mean().data
             wc_loss = temp_nominal_loss
             
-            # print(f'Current gamma = {g}')
-            # print(f'Initial loss:{temp_nominal_loss}')
             
             # loop over target columns (for current node), find worst-case loss:
             for col in current_target_col:
@@ -647,39 +640,20 @@ class gd_FDRR(nn.Module):
             best_col_ind = np.argsort(local_loss)[-1]                
             wc_loss = np.max(local_loss)
             best_col = current_target_col[best_col_ind]
-
-            # Find Feature with WC loss over this iteration
-            # print(f'All losses = {local_loss}')
-            # print(f'WC Loss:{wc_loss}')
-
-            # if g == 2: stop_
             
-            # print(best_col)
-            # print(best_col_ind)
-            # print(torch.cat([current_target_col[0:best_col_ind], current_target_col[best_col_ind+1:]]))
-            
-            
-            # print(f'Nominal Loss:{temp_nominal_loss}')
-            # print(f'WC Loss:{wc_loss}')
-            # print(f'Feature:{best_col}')
-
-            # best_alpha[:,current_target_col[best_col_ind]] = 1
-            # current_target_col = torch.cat([current_target_col[0:best_col_ind], current_target_col[best_col_ind+1:]])   
-
-            # check if performance degrades enough, apply split
-            
-            if wc_loss > temp_nominal_loss:
-                # update
+            # !!!!! This approximates an equality constraint on the total budget
+            if self.budget_constraint == 'equality':
                 best_alpha[:,best_col] = 1
                 apply_split = True
-            
-            # best_alpha[:,best_col] = 1
-            # apply_split = True
+            elif self.budget_constraint == 'inequality':
+                
+                # check if performance degrades enough, apply split
+                if wc_loss > temp_nominal_loss:
+                    # update
+                    best_alpha[:,best_col] = 1
+                    apply_split = True
                 
             
-            # if g == 2: 
-            #     print(best_alpha[0])
-            #     stop_  
             #update list of eligible columns
             if apply_split:
                 current_target_col = torch.cat([current_target_col[0:best_col_ind], current_target_col[best_col_ind+1:]])   
@@ -746,7 +720,7 @@ class gd_FDRR(nn.Module):
             
         return total_loss / len(loader.dataset)
     
-    def adversarial_epoch_train(self, loader, opt=None):
+    def adversarial_epoch_train(self, loader, opt=None, attack_type = 'greedy'):
         """Adversarial training/evaluation epoch over the dataset"""
         total_loss = 0.
         
@@ -761,18 +735,19 @@ class gd_FDRR(nn.Module):
         for X,y in loader:            
             #### Find adversarial example
             
-            # Greedy top-down heuristic
-            # alpha = self.missing_data_attack(X, y, gamma = self.gamma)
-            
-            # sample random features
-            feat_col = np.random.choice(np.arange(len(self.target_col)), replace = False, size = (self.gamma))
-            alpha = torch.zeros_like(X)
-            for c in feat_col: alpha[:,c] = 1
-
-            # alpha = torch.ones_like(X)*torch.FloatTensor(alpha_val).reshape(1,-1)
-
-            # # L1 attack with projected gradient descent            
-            # alpha = self.l1_norm_attack(X,y) 
+            if attack_type == 'greedy':
+                # Greedy top-down heuristic
+                # Works best when budget_constraint == 'inequality'
+                alpha = self.missing_data_attack(X, y, gamma = self.gamma)
+            elif attack_type == 'random_sample':
+                # sample random features            
+                # Approximates well the FDR-reformulation with budget equality constraint
+                feat_col = np.random.choice(np.arange(len(self.target_col)), replace = False, size = (self.gamma))
+                alpha = torch.zeros_like(X)
+                for c in feat_col: alpha[:,c] = 1
+            elif attack_type == 'l1_norm':                
+                # L1 attack with projected gradient descent            
+                alpha = self.l1_norm_attack(X,y) 
             
             # # Solve LP
             # for layer in self.model.children():
@@ -824,7 +799,8 @@ class gd_FDRR(nn.Module):
         
         return loss_i
             
-    def train_model(self, train_loader, val_loader, optimizer, epochs = 20, patience=5, verbose = 0, warm_start = False):
+    def train_model(self, train_loader, val_loader, optimizer, epochs = 20, patience=5, verbose = 0, warm_start = False, 
+                    attack_type = 'greedy'):
         
         best_train_loss = float('inf')
         best_val_loss = float('inf')
@@ -870,26 +846,7 @@ class gd_FDRR(nn.Module):
             alpha_tensor = torch.cat(alpha_tensor_list, dim = 0)
             
             for epoch in range(epochs):
-
-                # average_train_loss = self.adversarial_epoch_train(train_loader, optimizer)                
-                # average_train_loss = self.adversarial_epoch_train_heur(train_loader, alpha_tensor, optimizer)                
-    
-                # if (verbose != -1)and(epoch%25 == 0):
-                #     print(f"Epoch [{epoch + 1}/{epochs}] - Train Loss: {average_train_loss:.4f}")
-                
-                # if average_train_loss < best_train_loss:
-                #     best_train_loss = average_train_loss
-                #     best_weights = copy.deepcopy(self.state_dict())
-                #     early_stopping_counter = 0
-                # else:
-                #     early_stopping_counter += 1
-                #     if early_stopping_counter >= patience:
-                #         print("Early stopping triggered.")
-                #         # recover best weights
-                #         self.load_state_dict(best_weights)
-                #         return    
-
-                average_train_loss = self.adversarial_epoch_train(train_loader, optimizer)                
+                average_train_loss = self.adversarial_epoch_train(train_loader, optimizer, attack_type)                
                 val_loss = self.adversarial_epoch_train(val_loader)
     
                 if (verbose != -1)and(epoch%25 == 0):