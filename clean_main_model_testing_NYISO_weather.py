81c3f5585e064025c4a6a7e586dbb87ce17f7b92
akylasstrat <akylas.strat@hotmail.gr>
2024-10-04 15:00:36 +0100

    mnar results

--- clean_main_model_testing_NYISO_weather.py
+++ clean_main_model_testing_NYISO_weather.py
@@ -70,7 +70,7 @@ metadata_df = pd.read_csv(f'{cd}\\data\\wind_meta.csv', index_col = 0)
 #%%
 freq = '15min'
 target_park = 'Noble Clinton'
-horizon = 16
+horizon = 24
 
 config['save'] = True
 # min_lag: last known value, which defines the lookahead horizon (min_lag == 2, 1-hour ahead predictions)
@@ -241,7 +241,7 @@ check_length['Length'] = block_length[block_length.diff()!=0]
 check_length['Missing'] = miss_ind[block_length.diff()!=0]
 check_length.groupby('Missing').mean()
 #%%
-
+stop_
 print('Test for MCAR mechanism')
 for perc in percentage:
     for iter_ in range(iterations):
@@ -397,7 +397,7 @@ for perc in percentage:
     if config['save']:
         mae_df.to_csv(f'{cd}\\new_results\\{freq}_{target_park}_MCAR_{min_lag}_steps_MAE_results_weather.csv')
         rmse_df.to_csv(f'{cd}\\new_results\\{freq}_{target_park}_MCAR_{min_lag}_steps_RMSE_results_weather.csv')
-#%%
+
     ls_models = ['LR', 'FA-LEARN-LDR-LR-10', 'FA-FIXED-LDR-LR', 'FA-FIXED-LR', 'FA-LEARN-LR-10']
     rmse_df.groupby(['percentage']).mean()[ls_models].plot()
 
@@ -499,77 +499,74 @@ for iter_ in range(iterations):
                 imp_X[np.where(miss_ind[:,j] == 1), j] = mean_imput_values[j]
     
     
-    ############ Impute-then-Regress
-    
-    #### Persistence
-    pers_pred = imp_X[f'{target_park}_{min_lag}'].values.reshape(-1,1)
-    temp_Predictions['Pers'] = pers_pred.reshape(-1)
-            
-    #### LS model
-    lr_pred = projection(lr_model.predict(imp_X).reshape(-1,1))
-    temp_Predictions['LS'] = lr_pred.reshape(-1)
-            
-    #### LASSO
-    lasso_pred = projection(lasso_model.predict(imp_X).reshape(-1,1))
-    temp_Predictions['Lasso'] = lasso_pred.reshape(-1)
-
-    #### RIDGE
-    l2_pred = projection(ridge_model.predict(imp_X).reshape(-1,1))
-    temp_Predictions['Ridge'] = l2_pred.reshape(-1)
+        ############ Impute-then-Regress
         
-    #### LAD model
-    lad_pred = projection(lad_model.predict(imp_X).reshape(-1,1))
-    temp_Predictions['LAD'] = lad_pred.reshape(-1)
-
-    #### MLPimp
-    mlp_pred = mlp_model.predict(torch.FloatTensor(imp_X.values)).reshape(-1,1)
-    temp_Predictions['NN'] = mlp_pred.reshape(-1)
+        #### Persistence
+        pers_pred = imp_X[f'{target_park}_{min_lag}'].values.reshape(-1,1)
+        temp_Predictions['Pers'] = pers_pred.reshape(-1)
+                
+        #### LS model
+        lr_pred = projection(lr_model.predict(imp_X).reshape(-1,1))
+        temp_Predictions['LR'] = lr_pred.reshape(-1)
+                
+        #### LASSO
+        lasso_pred = projection(lasso_model.predict(imp_X).reshape(-1,1))
+        temp_Predictions['Lasso'] = lasso_pred.reshape(-1)
+    
+        #### RIDGE
+        l2_pred = projection(ridge_model.predict(imp_X).reshape(-1,1))
+        temp_Predictions['Ridge'] = l2_pred.reshape(-1)
             
-    ######### Adversarial Models
-    
-    #### Finite Adaptability - Fixed Partitions
-    ## LS model
-    FA_fixed_LS_pred = FA_fixed_LS_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
-    FA_fixed_LS_pred = projection(FA_fixed_LS_pred)
-    temp_Predictions['FA-fixed-LS'] = FA_fixed_LS_pred.reshape(-1)
+        #### LAD model
+        lad_pred = projection(lad_model.predict(imp_X).reshape(-1,1))
+        temp_Predictions['LAD'] = lad_pred.reshape(-1)
 
-    ## NN model
-    FA_fixed_NN_pred = FA_fixed_NN_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
-    FA_fixed_NN_pred = projection(FA_fixed_NN_pred)
-    temp_Predictions['FA-fixed-NN'] = FA_fixed_NN_pred.reshape(-1)
-
-    #### Finite Adaptability - Linear - Fixed Partitions
-    ## LS model
-    FA_lin_fixed_LS_pred = FA_lin_fixed_LS_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
-    FA_lin_fixed_LS_pred = projection(FA_lin_fixed_LS_pred)
-    temp_Predictions['FA-lin-fixed-LS'] = FA_lin_fixed_LS_pred.reshape(-1)
-
-    ## NN model
-    FA_lin_fixed_NN_pred = FA_lin_fixed_NN_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
-    FA_lin_fixed_NN_pred = projection(FA_lin_fixed_NN_pred)
-    temp_Predictions['FA-lin-fixed-NN'] = FA_lin_fixed_NN_pred.reshape(-1)
-
-    #### FINITE-RETRAIN-LAD and LS
-    
-    FA_lin_greedy_NN_pred = FA_lin_greedy_NN_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
-    FA_lin_greedy_NN_pred = projection(FA_lin_greedy_NN_pred)
-    temp_Predictions['FA-lin-greedy-NN'] = FA_lin_greedy_NN_pred.reshape(-1)
-
-    #### FA-Fixed-LS and NN
-    FA_greedy_LS_pred = FA_greedy_LS_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
-    FA_greedy_LS_pred = projection(FA_greedy_LS_pred)
-    temp_Predictions['FA-greedy-LS'] = FA_greedy_LS_pred.reshape(-1)
-
-    FA_greedy_NN_pred = FA_greedy_NN_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
-    FA_greedy_NN_pred = projection(FA_greedy_NN_pred)
-    temp_Predictions['FA-greedy-NN'] = FA_greedy_NN_pred.reshape(-1)
-
-    #### FINITE-RETRAIN-LAD and LS
-    for number_splits in FA_lin_greedy_LS_models_dict.keys():
+        #### MLPimp
+        mlp_pred = mlp_model.predict(torch.FloatTensor(imp_X.values)).reshape(-1,1)
+        temp_Predictions['NN'] = mlp_pred.reshape(-1)
+                
+        ######### Adversarial Models
         
-        FA_lin_greedy_LS_pred = FA_lin_greedy_LS_models_dict[number_splits].predict(miss_X_zero.values, miss_X.isna().values.astype(int))
-        FA_lin_greedy_LS_pred = projection(FA_lin_greedy_LS_pred)
-        temp_Predictions[f'FA-lin-greedy-LS-{number_splits}'] = FA_lin_greedy_LS_pred.reshape(-1)
+        #### Finite Adaptability - Fixed Partitions
+        ## LS model
+        FA_FIXED_LR_pred = FA_FIXED_LR_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
+        temp_Predictions['FA-FIXED-LR'] = projection(FA_FIXED_LR_pred).reshape(-1)
+
+        ## NN model
+        FA_FIXED_NN_pred = FA_FIXED_NN_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
+        temp_Predictions['FA-FIXED-NN'] = projection(FA_FIXED_NN_pred).reshape(-1)
+
+        #### Finite Adaptability - Fixed Partitions - LDR
+        ## LS model
+        FA_FIXED_LDR_LR_pred = FA_FIXED_LDR_LR_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
+        temp_Predictions['FA-FIXED-LDR-LR'] = projection(FA_FIXED_LDR_LR_pred).reshape(-1)
+
+        ## NN model
+        FA_FIXED_LDR_NN_pred = FA_FIXED_LDR_NN_model.predict(miss_X_zero.values, miss_X.isna().values.astype(int))
+        temp_Predictions['FA-FIXED-LDR-NN'] = projection(FA_FIXED_LDR_NN_pred).reshape(-1)
+
+
+        #### Finite Adaptability - Learned Partitions
+        
+        ### LDR - LS//NN
+        
+        for number_splits in FA_LEARN_LDR_LR_models_dict.keys():            
+            temp_FA_LEARN_LDR_LR_pred = FA_LEARN_LDR_LR_models_dict[number_splits].predict(miss_X_zero.values, miss_X.isna().values.astype(int))
+            temp_Predictions[f'FA-LEARN-LDR-LR-{number_splits}'] = projection(temp_FA_LEARN_LDR_LR_pred).reshape(-1)
+
+        for number_splits in FA_LEARN_LDR_NN_models_dict.keys():            
+            temp_FA_LEARN_LDR_NN_pred = FA_LEARN_LDR_NN_models_dict[number_splits].predict(miss_X_zero.values, miss_X.isna().values.astype(int))
+            temp_Predictions[f'FA-LEARN-LDR-NN-{number_splits}'] = projection(temp_FA_LEARN_LDR_NN_pred).reshape(-1)
+        
+        # ### Static models, no linear decision rules - LS//NN
+        
+        for number_splits in FA_LEARN_LR_models_dict.keys():            
+            temp_FA_LEARN_LR_pred = FA_LEARN_LR_models_dict[number_splits].predict(miss_X_zero.values, miss_X.isna().values.astype(int))
+            temp_Predictions[f'FA-LEARN-LR-{number_splits}'] = projection(temp_FA_LEARN_LR_pred).reshape(-1)
+
+        for number_splits in FA_LEARN_NN_models_dict.keys():            
+            temp_FA_LEARN_NN_pred = FA_LEARN_NN_models_dict[number_splits].predict(miss_X_zero.values, miss_X.isna().values.astype(int))
+            temp_Predictions[f'FA-LEARN-NN-{number_splits}'] = projection(temp_FA_LEARN_NN_pred).reshape(-1)
     
     for m in models:
         temp_df[m] = [mae(temp_Predictions[m].values, Target.values)]
@@ -580,17 +577,18 @@ for iter_ in range(iterations):
     rmse_df = pd.concat([rmse_df, temp_df])
     
     run_counter += 1
-#%%
+
 if config['save']:
     mae_df.to_csv(f'{cd}\\new_results\\{freq}_{target_park}_MNAR_{min_lag}_steps_MAE_results_weather.csv')
     rmse_df.to_csv(f'{cd}\\new_results\\{freq}_{target_park}_MNAR_{min_lag}_steps_RMSE_results_weather.csv')
-    
-ls_models = ['LS', 'FA-greedy-LS', 'FA-fixed-LS', 'FA-lin-fixed-LS', 'FA-lin-greedy-LS-10']
-rmse_df.groupby(['percentage']).mean()[ls_models].plot()
+#%%    
+ls_models = ['LR', 'FA-LEARN-LDR-LR-10', 'FA-LEARN-LR-10', 'FA-FIXED-LR', 'FA-FIXED-LDR-LR']
+rmse_df.groupby(['percentage']).mean()[ls_models].plot(kind='bar')
 
-nn_models = ['NN', 'FA-greedy-NN', 'FA-fixed-NN', 'FA-lin-fixed-NN', 'FA-lin-greedy-NN']
-rmse_df.groupby(['percentage']).mean()[nn_models].plot()
+nn_models = ['NN', 'FA-LEARN-LDR-NN-10', 'FA-LEARN-NN-10', 'FA-FIXED-NN', 'FA-FIXED-LDR-NN']
+rmse_df.groupby(['percentage']).mean()[nn_models].plot(kind='bar')
 
+stop_
 #%%%%%%%%%% CENSORING: Censor data above a specific threshold
 #### Note: This falls under MNAR, may replace the current MNAR results (TBD)
 
